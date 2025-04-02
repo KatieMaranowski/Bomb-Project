6b8dfdf9191ec251bcfb41837cd722cfe53059ac
KatieMaranowski <katherine.maranowski@spartans.ut.edu>
2025-04-02 13:50:26 -0400

    Update bombGUItemplate.py

--- bombGUItemplate.py
+++ bombGUItemplate.py
@@ -1,4 +1,10 @@
 ########### GUI Template ##############
 
 from tkinter import *
+button = 0 # Status of button (0 = not complete, 1 = complete)
+keypad = 0 # Status of keypad 
 
+window = Tk()
+text = Label(window,text="Button {}".format(button))
+text.pack()
+window.mainloop()
