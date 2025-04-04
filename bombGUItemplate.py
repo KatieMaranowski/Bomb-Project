ab05404917acbdc9cc02e3c8625f742ef850773f
ASodaCan <84260591+ASodaCan@users.noreply.github.com>
2025-04-04 13:10:22 -0400

    Update bombGUItemplate.py

--- bombGUItemplate.py
+++ bombGUItemplate.py
@@ -1,10 +1,23 @@
-########### GUI Template ##############
-
 from tkinter import *
-button = 0 # Status of button (0 = not complete, 1 = complete)
-keypad = 0 # Status of keypad 
+
+class App(Frame):
+    def __init__(self, window):
+        Frame.__init__(self, window)
+        self.label1 = Label(window, text='Email:', bg='#E200E6')
+        self.label2 = Label(window, text='Password:', bg='#E200E6')
+        self.label1.grid(row=0, column=0)
+        self.label2.grid(row=1, column=0)
+        self.emailBox = Entry(window)
+        self.pwBox = Entry(window)
+        self.emailBox.grid(row=0, column=1)
+        self.pwBox.grid(row=1, column=1)
+        self.loginBtn = Button(window, text='Login', command=None)
+        self.loginBtn.grid(row=2, columnspan=2)
 
 window = Tk()
-text = Label(window,text="Button {}".format(button))
-text.pack()
+window.title("Adding an Image")
+app = App(window)
+window.config(bg='#E200E6')
+
 window.mainloop()
+
