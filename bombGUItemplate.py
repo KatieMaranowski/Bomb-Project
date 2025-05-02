8dcc88578248cd2aabc58c2ff1710212cb1de2b6
KatieMaranowski <katherine.maranowski@spartans.ut.edu>
2025-05-02 12:51:33 -0400

    format for virus image

--- bombGUItemplate.py
+++ bombGUItemplate.py
@@ -1,15 +1,19 @@
 
-
 from tkinter import *
 
 keypad_count = 0 # This will show as 0-3 because you have to complete three different codes to finish the keypad phase
 button_status = 0 #0 means button phase not complete, 1 means button phase complete
 wires_status = 0 # 0 means wire not complete, 1 means wire complete
 font_example = ("Arial", 16, "bold")
+toggle_status = 0
 
 def evil_messages(keypad_count):
   messages = ["Nothing can stop me now","THIS ISN'T EVEN MY FINAL FORM!","Nice try!","[from evil_libraries import maniacal_laugh","Didn't anyone ever teach you internet safety?"]
-  text = random.choice(messages)
+  while True:
+      text = random.choice(messages)
+      sleep(1)
+      text = random.choice(messages)
+      sleep(1)
 
 if keypad_count == 0:
     text = "generic evil message"
@@ -25,28 +29,29 @@ class App(Frame):
     def __init__(self, window):
         global keypad_count
         global text
+        global wires_status
+        global button_status
+        global toggle_status
+    
         Frame.__init__(self, window)
         self.keypadlabel = Label(window, text='Keypad:  {}/3'.format(keypad_count), fg = '#00008B',bg='#D3D3D3',font=font_example)
         self.buttonlabel = Label(window, text='Button:  {}'.format(button_status), fg = '#00008B', bg='#D3D3d3',font=font_example)
         self.wireslabel = Label(window, text='Wires:  {}'.format(wires_status), fg = '#00008B', bg='#D3D3D3',font=font_example)
+        self.toggleslabel = Label(window, text='Toggles:  {}'.format(toggle_status), fg = '#00008B', bg='#D3D3D3',font=font_example)
         self.textbox = Text(window,  width = 30, height = 10,fg = '#00008B', bg = '#FFFFFF', font = font_example, wrap=WORD)
         textboxtext = "{}".format(text)
         self.textbox.insert(END, textboxtext)
         self.viruscharacter = PhotoImage(file="viruscharacter.gif")
         self.viruscharacter2 = Label(window, image=self.viruscharacter, bg='#D3D3D3')
         
-        self.keypadlabel.grid(row=0, column=3,ipadx=50)
-        self.buttonlabel.grid(row=0, column=5,ipadx=50)
-        self.wireslabel.grid(row=0, column=10,ipadx=50)
-        self.textbox.grid(row=1, column = 1, columnspan=50, rowspan= 50,pady=300, padx= 0,sticky="NW")
+        self.keypadlabel.grid(row=0, column=0)
+        self.buttonlabel.grid(row=1, column=0)
+        self.wireslabel.grid(row=2, column=0)
+        self.toggleslabel.grid(row=3,column=0)
+        self.textbox.grid(row=1, column = 1,rowspan = 2,sticky="NW")
         self.textbox.grid_propagate(False)
         
-        self.viruscharacter2.grid(row=1,column=0, padx = 0, pady = 350)
-        #self.text_frame = Frame(self, width=2, height=2)
-        #self.speechbox = Text(self.text_frame,bg="red", state=DISABLED, wrap=WORD) 
-        #self.speechbox.pack(fill=Y, expand=1)
-        #self.text_frame.pack(side=TOP, fill=Y)
-        #self.text_frame.pack_propagate(False)
+        self.viruscharacter2.grid(row=1,column=3,rowspan=3)
         
 window = Tk()
 window.title("Adding an Image")
